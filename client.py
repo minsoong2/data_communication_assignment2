c66a8f993cbe3c2dbbc224fdf94f8d0cad9453f8
minsoong2 <happyms817@gmail.com>
2023-11-02 22:57:25 +0900

    231102

---
+++ client.py
@@ -0,0 +1,42 @@
+import socket
+import random
+import time
+import threading
+
+
+# server_ip = "ec2-15-164-95-106.ap-northeast-2.comp ute.amazonaws.com"
+# server_port = 55555
+server_ip = "127.0.0.1"
+server_port = 8888
+
+
+def client(client_id):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((server_ip, server_port))
+    print(f"Client {client_id}: start!!")
+
+    try:
+        with open(f"Client{client_id}.txt", "w") as f:
+
+    except ConnectionResetError:
+        with open(f"Client{client_id}.txt", "w") as f:
+            e_line = f"Client {client_id}: Connection to the server was forcibly closed."
+            print(e_line)
+            f.write(e_line + '\n')
+    except KeyboardInterrupt:
+        pass
+    finally:
+        client_socket.close()
+
+if __name__ == "__main__":
+    client_threads = []
+    for i in range(1, 5):
+        client_thread = threading.Thread(target=client, args=(i,))
+        client_threads.append(client_thread)
+
+    for thread in client_threads:
+        thread.start()
+        print(thread, "start!!!")
+
+    for thread in client_threads:
+        thread.join()
---
+++ server.py
@@ -0,0 +1,83 @@
+import socket
+import threading
+import random
+import time
+
+
+ip = '127.0.0.1'
+port = 8888
+
+system_clock = 0
+server_operating_time = 60
+total_sum = 0
+f = open("Server.txt", "w")
+
+
+def update_system_clock():
+    global system_clock
+    while system_clock < server_operating_time:
+        time.sleep(1)
+        system_clock += 1
+
+
+def handle_client(client_socket):
+    global system_clock
+    try:
+        while system_clock < server_operating_time:
+            system_clock += 1
+    except Exception as e:
+        e_line = f"Error: {e}"
+        print(e_line)
+        f.write(e_line + '\n')
+    finally:
+        client_socket.close()
+
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((ip, port))
+    server.listen(4)
+    server.settimeout(1)
+
+    listen = "Server is listening..."
+    print(listen)
+    f.write(listen + '\n')
+
+    threads = []
+    clock_thread = threading.Thread(target=update_system_clock)
+    clock_thread.daemon = True
+    clock_thread.start()
+
+    client_count = 0
+
+    while system_clock < server_operating_time:
+        if system_clock >= server_operating_time:
+            break
+        try:
+            client_socket, client_address = server.accept()
+            accept = f"Accepted connection from {client_address}"
+            print(accept)
+            f.write(accept + '\n')
+            client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+            client_handler.start()
+            threads.append(client_handler)
+            client_count += 1
+        except socket.timeout:
+            pass
+
+    for thread in threads:
+        thread.join()
+
+    print_system_clock = f"end_time: {system_clock}"
+    print(print_system_clock)
+    f.write(print_system_clock + '\n')
+    server.close()
+    print("Server closed...")
+    f.write("Server closed...")
+    if client_count == 0:
+        print("No clients connected during the 30 seconds.")
+        f.write("No clients connected during the 30 seconds.")
+
+
+if __name__ == "__main__":
+    main()
