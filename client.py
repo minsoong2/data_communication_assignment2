e6d04459f69749fa1bf73f1711e48a4475c8caea
minsoong2 <happyms817@gmail.com>
2023-11-08 18:56:24 +0900

    231108

--- client.py
+++ client.py
@@ -2,7 +2,8 @@ import socket
 import random
 import time
 import threading
-
+import numpy as np
+import re
 
 # server_ip = "ec2-15-164-95-106.ap-northeast-2.comp ute.amazonaws.com"
 # server_port = 55555
@@ -10,23 +11,86 @@ server_ip = "127.0.0.1"
 server_port = 8888
 
 
-def client(client_id):
+def generate_random_matrix():
+    return np.random.randint(0, 101, size=(10, 10))
+
+
+def perform_vector_multiplication(row_vector, col_vector):
+    if row_vector is not None and col_vector is not None:
+        result = np.dot(row_vector, col_vector)
+        return result
+    else:
+        return None
+
+
+def client(c_id):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((server_ip, server_port))
-    print(f"Client {client_id}: start!!")
+    print(f"Client {client_socket.getsockname()[1]}: Connected to the server")
 
     try:
-        with open(f"Client{client_id}.txt", "w") as f:
-            print("!!!")
+        data = client_socket.recv(1024).decode()
+        client_id = f"{client_socket.getsockname()[1]}"
+        row_col_1, row_col_2 = [], []
+
+        if client_id in data and "Request idx_data -> Row" in data:
+            print(client_id, data)
+            match = re.search(r"Row \[(\d+), (\d+)\]", data)
+            row_indices = [int(match.group(1)), int(match.group(2))]
+            print(f"Client {client_socket.getsockname()[1]} Received row_idx_data:", row_indices)
+            client_matrix = generate_random_matrix()
+            print(f"Client {client_socket.getsockname()[1]} matrix: ", client_matrix)
+            row_data = client_matrix[row_indices[0]]
+            row_data_2 = client_matrix[row_indices[1]]
+            row_data_str = f"Client {client_socket.getsockname()[1]} - Row: {' '.join(map(str, row_data))}, {' '.join(map(str, row_data_2))}"
+            print(row_data_str)
+            client_socket.send(row_data_str.encode())
+
+        elif client_id in data and "Request idx_data -> Col" in data:
+            print(client_id, data)
+            match = re.search(r"Col \[(\d+), (\d+)\]", data)
+            col_indices = [int(match.group(1)), int(match.group(2))]
+            print(f"Client {client_socket.getsockname()[1]} Received col_idx_data:", col_indices)
+            client_matrix = generate_random_matrix()
+            print(f"Client {client_socket.getsockname()[1]} matrix: ", client_matrix)
+            col_data = client_matrix[:, col_indices[0]]
+            col_data_2 = client_matrix[:, col_indices[1]]
+            col_data_str = f"Client {client_socket.getsockname()[1]} - Col: {' '.join(map(str, col_data))}, {' '.join(map(str, col_data_2))}"
+            print(col_data_str)
+            client_socket.send(col_data_str.encode())
+
+        else:
+            if client_id in data and "Send data -> Row1, Col1:" in data:
+                print(client_id, data)
+                print()
+                numbers_str = re.findall(r'Send data -> Row1, Col1:\s+(.*)$', data)
+                print(numbers_str)
+                if numbers_str:
+                    numbers_str = numbers_str[0]
+                    numbers_list = [int(num) for num in numbers_str.split()]
+                    row_col_1.append(numbers_list)
+
+            elif client_id in data and "Send data -> Row2, Col2:" in data:
+                print(client_id, data)
+                print()
+                numbers_str = re.findall(r'Row1:\s+(.*)$', data)
+                if numbers_str:
+                    numbers_str = numbers_str[0]
+                    numbers_list = [int(num) for num in numbers_str.split()]
+                    row_col_1.append(numbers_list)
+
+        # print(len(row_col_1), row_col_1)
+        # print(len(row_col_2), row_col_2)
+        # result_non_selected_client1 = perform_vector_multiplication(row_col_1[0], row_col_1[1])
+        # result_non_selected_client2 = perform_vector_multiplication(row_col_2[0], row_col_2[1])
 
     except ConnectionResetError:
-        with open(f"Client{client_id}.txt", "w") as f:
-            e_line = f"Client {client_id}: Connection to the server was forcibly closed."
-            print(e_line)
-            f.write(e_line + '\n')
+        e_line = f"Client {client_socket.getsockname()[1]}: Connection to the server was forcibly closed."
+        print(e_line)
     except KeyboardInterrupt:
         pass
     finally:
+        print(f"Client {client_socket.getsockname()[1]}: Connection closed")
         client_socket.close()
 
 
@@ -36,6 +100,7 @@ if __name__ == "__main__":
         client_thread = threading.Thread(target=client, args=(i,))
         client_threads.append(client_thread)
 
+    print(client_threads)
     for thread in client_threads:
         thread.start()
         print(thread, "start!!!")
--- server.py
+++ server.py
@@ -8,23 +8,96 @@ ip = '127.0.0.1'
 port = 8888
 
 system_clock = 0
-round = 100
-total_sum = 0
+round_cnt = 1
+matrices = [np.zeros((10, 10)) for _ in range(6)]
+
+client_count = 0
+client_sockets = []
+client_semaphore = threading.Semaphore(0)
+
 f = open("Server.txt", "w")
 
 
 def update_system_clock():
     global system_clock
-    while system_clock < round:
+    while round_cnt <= 100:
         time.sleep(1)
         system_clock += 1
 
 
 def handle_client(client_socket):
-    global system_clock
+    global system_clock, round_cnt, matrices, client_count
     try:
-        while system_clock < round:
-            system_clock += 1
+        client_sockets.append(client_socket)
+        client_count += 1
+        if client_count == 4:
+            client_semaphore.release()
+        client_semaphore.acquire()
+
+        client_sockets.sort(key=lambda x: x.getpeername()[1])
+
+        matrices = [np.zeros((10, 10)) for _ in range(6)]
+        clients = random.sample(range(1, 5), 2)
+        rows = [random.randint(0, 9) for _ in range(2)]
+        cols = [random.randint(0, 9) for _ in range(2)]
+        print(len(client_sockets))
+
+        for idx, c_socket in enumerate(client_sockets):
+            if idx == clients[0] - 1:
+                request_row_idx_data = f"Round {round_cnt}: Client {c_socket.getpeername()} - Request idx_data -> Row {rows}"
+                print(request_row_idx_data)
+                f.write(request_row_idx_data + '\n')
+                c_socket.send(request_row_idx_data.encode())
+                receive_data = c_socket.recv(1024).decode()
+                print(receive_data)
+                if "Row" in receive_data:
+                    data_list = receive_data.split(":")[1].strip().split(", ")
+                    print("row_data_list", data_list)
+                    data_row1 = data_list[0]
+                    data_row2 = data_list[1]
+
+                elif "Col" in receive_data:
+                    data_list = receive_data.split(":")[1].strip().split(", ")
+                    print("col_data_list", data_list)
+                    data_col1 = data_list[0]
+                    data_col2 = data_list[1]
+
+            elif idx == clients[1] - 1:
+                request_col_idx_data = f"Round {round_cnt}: Client {c_socket.getpeername()} - Request idx_data -> Col {cols}"
+                print(request_col_idx_data)
+                f.write(request_col_idx_data + '\n')
+                c_socket.send(request_col_idx_data.encode())
+                receive_data2 = c_socket.recv(1024).decode()
+                print(receive_data2)
+                if "Row" in receive_data2:
+                    data_list = receive_data2.split(":")[1].strip().split(", ")
+                    print("row_data_list", data_list)
+                    data_row1 = data_list[0]
+                    data_row2 = data_list[1]
+
+                elif "Col" in receive_data2:
+                    data_list = receive_data2.split(":")[1].strip().split(", ")
+                    print("col_data_list", data_list)
+                    data_col1 = data_list[0]
+                    data_col2 = data_list[1]
+
+        non_selected_clients = [c for c in range(1, 5) if c not in clients]
+        print(clients, non_selected_clients)
+        for idx, c_socket in enumerate(client_sockets):
+            if idx == non_selected_clients[0] - 1:
+                send_row_col_data1 = f"Round {round_cnt}: Client {c_socket.getpeername()} - Send data -> Row1, Col1: [{data_row1}], [{data_col1}]"
+                print(send_row_col_data1)
+                c_socket.send(send_row_col_data1.encode())
+                # 무조건 여기에 행렬 결과값 받기!!!!!!!!
+            elif idx == non_selected_clients[1] - 1:
+                send_row_col_data2 = f"Round {round_cnt}: Client {c_socket.getpeername()} - Send data -> Row2, Col2: [{data_row2}], [{data_col2}]"
+                print(send_row_col_data2)
+                c_socket.send(send_row_col_data2.encode())
+                # 무조건 여기에 행렬 결과값 받기!!!!!!!!
+
+        system_clock += 1
+        round_cnt += 1
+
     except Exception as e:
         e_line = f"Error: {e}"
         print(e_line)
@@ -43,40 +116,46 @@ def main():
     print(listen)
     f.write(listen + '\n')
 
-    threads = []
     clock_thread = threading.Thread(target=update_system_clock)
     clock_thread.daemon = True
     clock_thread.start()
 
-    client_count = 0
-
-    while system_clock < round:
-        if system_clock >= round:
-            break
+    client_threads = []
+    while round_cnt <= 100:
         try:
             client_socket, client_address = server.accept()
             accept = f"Accepted connection from {client_address}"
             print(accept)
             f.write(accept + '\n')
+
             client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+            client_threads.append(client_handler)
             client_handler.start()
-            threads.append(client_handler)
-            client_count += 1
         except socket.timeout:
             pass
 
-    for thread in threads:
+    for thread in client_threads:
         thread.join()
 
     print_system_clock = f"end_time: {system_clock}"
     print(print_system_clock)
     f.write(print_system_clock + '\n')
+
+    for i, matrix in enumerate(matrices):
+        matrix_data = f"Round {round_cnt}: Matrix {i}\n{matrix}"
+        print(matrix_data)
+        f.write(matrix_data + '\n')
+
     server.close()
     print("Server closed...")
     f.write("Server closed...")
     if client_count == 0:
         print("No clients connected during the 30 seconds.")
         f.write("No clients connected during the 30 seconds.")
+
+    if round_cnt > 100:
+        print("Operation completed.")
+        f.write("Operation completed.")
 
 
 if __name__ == "__main__":
